9e323838fb37319d0562e7122eb42fbd1648046d
Nurbek Orozaliev <nurik878878@yandex.ru>
2023-05-10 17:13:38 +0000

    make a deployment of the project

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,17 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv 
+ 
+load_dotenv() 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.7.79', '127.0.0.1', 'localhost', 'kittygramproject.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -61,9 +65,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -89,11 +90,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = 'static_backend/' 
+STATIC_ROOT = BASE_DIR / 'static_backend' 
+ 
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = '/var/www/kittygram/media' 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
